81280b7a10429064b40f3aa6cbd3218ea959a796
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-05 18:48:08 +0100

    Adds title

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,8 +1,8 @@
 export default function CalculatorPage() {
  return (
 	<div className="flex w-full justify-center">
-		<main className="w-[1200px]">
-			Foo
+		<main className="w-[1200px] py-10">
+			<h1 className="text-xl">Beräkna din månadskostand</h1>
 		</main>
 	</div>
 )
