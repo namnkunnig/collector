c7be7cc6ebb7247e2dde7687aa73152d751610a8
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-06 23:54:49 +0100

    Adds slider color

---
+++ src/components/RangedInput.tsx
@@ -0,0 +1,21 @@
+export default function RangedInput() {
+  return (
+    <div className="w-full relative">
+      <input type="range" min="0" max="1000" step="100" id="slider" defaultValue={0} className="w-full" onInput={InputHandler} />
+      <div id="selector">
+        <div id="button"></div>
+      </div>
+      <div className="flex">
+        <div id="progress"></div>
+      </div>
+    </div>
+  )
+
+  function InputHandler(e: React.MouseEvent<HTMLInputElement>) {
+    const button = document.getElementById('selector')
+    const progress = document.getElementById('progress')
+    if (!button || !progress) return
+    button.style.left = (parseInt(e.currentTarget?.value) / 10) * 0.96 + '%'
+    progress.style.width = (parseInt(e.currentTarget?.value) / 10) * 0.96 + '%'
+  }
+}
--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,11 +1,11 @@
+import RangedInput from '../components/RangedInput'
+
 export default function CalculatorPage() {
   return (
     <div className="flex w-full justify-center">
       <main className="w-[1200px] py-10">
         <h1 className="text-xl mb-3">Beräkna din månadskostnad</h1>
-        <div className="">
-          <input id="slider" type="range" min="20000" max="200000" step="10000" className="w-full appearance-none h-7 bg-transparent rounded-full" />
-        </div>
+        <RangedInput></RangedInput>
       </main>
     </div>
   )
