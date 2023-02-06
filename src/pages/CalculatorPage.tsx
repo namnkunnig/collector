29aa3da564ef904e083bf2032447918a6401f8fa
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-06 13:52:22 +0100

    Configuration

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,9 +1,12 @@
 export default function CalculatorPage() {
- return (
-	<div className="flex w-full justify-center">
-		<main className="w-[1200px] py-10">
-			<h1 className="text-xl">Beräkna din månadskostand</h1>
-		</main>
-	</div>
-)
+  return (
+    <div className="flex w-full justify-center">
+      <main className="w-[1200px] py-10">
+        <h1 className="text-xl mb-3">Beräkna din månadskostnad</h1>
+        <div className="">
+          <input id="slider" type="range" min="20000" max="200000" step="10000" className="w-full appearance-none h-7 bg-transparent rounded-full" />
+        </div>
+      </main>
+    </div>
+  )
 }
