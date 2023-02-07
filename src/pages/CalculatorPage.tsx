52fab49ccddb6a2a54235fcd0b864d3cf3bd0d99
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-07 21:38:38 +0100

    Adds tooltip

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,10 +1,14 @@
 import RangedInput from '../components/RangedInput'
+import ToolTip from '../components/ToolTip'
 
 export default function CalculatorPage() {
   return (
     <div className="flex w-full justify-center">
-      <main className="w-[1200px] py-10">
-        <h1 className="text-xl mb-8 font-semibold">Lånekalkyl</h1>
+      <main className="w-[1000px] py-10">
+        <div className="flex justify-between">
+          <h1 className="text-xl mb-8 font-semibold">Lånekalkyl</h1>
+          <ToolTip text={'12345 SEK / mån'}></ToolTip>
+        </div>
         <h2 className="text-lg font-semibold mb-2">Lånebelopp</h2>
         <RangedInput defaultValue={100000} min={20000} max={200000} step={10000} unit="kr"></RangedInput>
         <h2 className="text-lg font-semibold mb-2">Lånetid</h2>
