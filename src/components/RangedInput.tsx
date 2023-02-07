f47191701964cd96e07c1d9522292d7a181f4a8c
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-07 20:59:36 +0100

    Props for RangedInput

--- src/components/RangedInput.tsx
+++ src/components/RangedInput.tsx
@@ -1,28 +1,44 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
-export default function RangedInput() {
+export default function RangedInput({ min, max, step, unit, defaultValue }: RangedInput) {
   const buttonRef = useRef(null)
   const sliderRef = useRef(null)
+  const [value, setValue] = useState(defaultValue)
+
   return (
-    <div className="w-full relative">
-      <input type="range" min="0" max="1000" step="100" id="slider" defaultValue={0} className="w-full" onInput={InputHandler} />
+    <div className="w-full relative h-12">
+      <input type="range" min={min} max={max} step={step} id="slider" defaultValue={0} className="w-full" onInput={inputHandler} />
       <div ref={buttonRef} className="h-12 w-12 absolute top-[-9px] z-20 bg-collector rounded-full border-2 border-solid border-white"></div>
-      <div ref={sliderRef} className="w-0 h-8 bg-collector absolute box-border rounded-l-full ml-5 text-right text-white pt-[0.8px] pr-10 z-10 rounded-none">
-        <span className="align-middle">1000 kr</span>
+      <div ref={sliderRef} className="w-0 h-8 bg-collector absolute box-border rounded-l-full ml-5 text-right text-white pt-[0.8px] pr-8 z-10 rounded-none">
+        <span className="align-middle">
+          {value} {unit}
+        </span>
       </div>
       <div className="border-[1px] border-solid border-slate-600 h-8 box-border rounded-full absolute w-[96%] ml-5 mr-[148px] text-right text-black pr-8">
-        <span className="align-middle">200 000 kr</span>
+        <span className="align-middle">
+          {max} {unit}
+        </span>
       </div>
     </div>
   )
 
-  function InputHandler(e: React.MouseEvent<HTMLInputElement>) {
+  function inputHandler(e: React.MouseEvent<HTMLInputElement>) {
+    const value = parseInt(e.currentTarget?.value)
+    setValue(value)
     const currentButton = buttonRef.current
     const currentProgress = sliderRef.current
     if (!currentButton || !currentProgress) return
     const button = currentButton as HTMLDivElement
     const slider = currentProgress as HTMLDivElement
-    button.style.left = (parseInt(e.currentTarget?.value) / 10) * 0.96 + '%'
-    slider.style.width = (parseInt(e.currentTarget?.value) / 10) * 0.96 + '%'
+    button.style.left = ((value - min) / ((max - min) / 100)) * 0.96 + '%'
+    slider.style.width = ((value - min) / ((max - min) / 100)) * 0.96 + '%'
   }
+}
+
+interface RangedInput {
+  min: number
+  max: number
+  step: number
+  unit: string
+  defaultValue: number
 }
--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -4,8 +4,11 @@ export default function CalculatorPage() {
   return (
     <div className="flex w-full justify-center">
       <main className="w-[1200px] py-10">
-        <h1 className="text-xl mb-3">Beräkna din månadskostnad</h1>
-        <RangedInput></RangedInput>
+        <h1 className="text-xl mb-8 font-semibold">Lånekalkyl</h1>
+        <h2 className="text-lg font-semibold mb-2">Lånebelopp</h2>
+        <RangedInput defaultValue={100000} min={20000} max={200000} step={10000} unit="kr"></RangedInput>
+        <h2 className="text-lg font-semibold mb-2">Lånetid</h2>
+        <RangedInput defaultValue={7} min={2} max={10} step={1} unit="år"></RangedInput>
       </main>
     </div>
   )
