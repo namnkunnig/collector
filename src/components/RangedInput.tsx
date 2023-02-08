9796cbfc252e528645ceea013f856203a9aeb8f9
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-08 23:02:04 +0100

    Styling

--- src/components/RangedInput.tsx
+++ src/components/RangedInput.tsx
@@ -15,12 +15,12 @@ export default function RangedInput({ min, max, step, unit, defaultValue, callba
     <div className="w-full relative h-12">
       <input ref={inputRef} type="range" min={0} max={max} step={step} defaultValue={defaultValue} className="w-full slider" onInput={inputHandler} />
       <div ref={buttonRef} className="h-12 w-12 absolute top-[-9px] z-20 bg-violet rounded-full border-2 border-solid border-white"></div>
-      <div ref={sliderRef} className={`w-0 h-8 bg-violet absolute box-border rounded-l-full ml-5 text-right text-white pt-[0.8px] pr-8 z-10 rounded-none`}>
+      <div ref={sliderRef} className={`w-0 h-8 bg-violet absolute box-border rounded-l-full text-right text-white pt-[0.8px] pr-6 z-10 rounded-none`}>
         <span className={`align-middle`}>
           {value.toLocaleString('sv-SE')} {unit}
         </span>
       </div>
-      <div className={`border-[1px] border-solid border-slate-600 h-8 box-border rounded-full absolute w-[96%] ml-5 mr-[148px] text-right text-black pr-8`}>
+      <div className={`border-[1px] border-solid border-slate-600 h-8 box-border w-full rounded-full absolute mr-[148px] text-right text-black pr-8`}>
         <span className={`align-middle`}>
           {max} {unit}
         </span>
@@ -45,8 +45,8 @@ export default function RangedInput({ min, max, step, unit, defaultValue, callba
     if (!currentButton || !currentProgress) return
     const button = currentButton as HTMLDivElement
     const slider = currentProgress as HTMLDivElement
-    button.style.left = (value / (max / 100)) * 0.96 + '%'
-    slider.style.width = (value / (max / 100)) * 0.96 + '%'
+    button.style.left = (value / (max / 100)) * 0.955 + '%'
+    slider.style.width = (value / (max / 100)) * 0.955 + 1 + '%'
   }
 }
 
