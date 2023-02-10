5e767098976ee18494ac671da62a1efcc1a19096
Magnus Kinnander <magnus.kinnander@gmail.com>
2023-02-10 22:28:13 +0100

    Refactor

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -2,7 +2,7 @@ export default function ToolTip({ text }: Tooltip) {
   return (
     <div
       className="md:w-64 w-40 bg-violet text-white p-4 flex flex-col justify-start text-center font-semibold rounded-md relative mb-6
-			after:block after:absolute after:-inset-1 after:-skew-y-[45deg] after:bg-violet after:left-12 after:w-6 after:h-12 after:top-16 md:after:top-10 after:-z-40"
+			after:block after:absolute after:bg-violet after:left-12 after:w-6 after:-bottom-3 after:h-6 after:-skew-y-[45deg]"
     >
       <div>Exempel på månadskostnad</div>
       <div>{text}</div>
